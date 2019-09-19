6ca96ae8afb9b1c47bb76fe25c79e28da458851c
Gilliot Raphael <raphael.gilliot@bordeaux-inp.fr>
2019-09-19 19:33:15 +0200

    TestJauge.java final version

--- TD1/TestJauge.java
+++ TD1/TestJauge.java
@@ -1,5 +1,7 @@
 class TestJauge {
 
+    private int nb_of_tests;
+
     static public void main(String[] argvs) {
 	boolean estMisAssertion = false;
 	assert estMisAssertion = true;
@@ -12,19 +14,28 @@ class TestJauge {
 	int nb_of_tests = 0;
 	TestJauge temp = new TestJauge();	
 	temp.testDansIntervalle();
-	System.out.println(".");
-	nb_of_tests ++;
+	temp.testAccompli();
 	temp.testDépartSupMax();
-	System.out.println(".");
-	nb_of_tests ++;
-	temp.testDéparEgalMax();
-	System.out.println(".");
-	nb_of_tests ++;
-	System.out.println("OK," + nb_of_tests + " tests passed.");
+	temp.testAccompli();
+	temp.testDépartEgalMax();
+	temp.testAccompli();
+	temp.testDépartNégatif();
+	temp.testAccompli();
+	temp.testDépartNul();
+	temp.testAccompli();
+	temp.testDecrementer();
+	temp.testAccompli();
+	temp.testIncrementer();
+	temp.testAccompli();
+	System.out.println("OK," + temp.nb_of_tests + " tests passed.");
     }
 
     public TestJauge() {
-	
+	nb_of_tests = 0;
+    }
+    private void testAccompli() {
+	System.out.println(".");
+	nb_of_tests ++;
     }
 
     public void testDansIntervalle() {
@@ -43,15 +54,74 @@ class TestJauge {
     /*******************************************
      Rapport de Bogue sur testDépartSupMax() :
      L'appel de estVert() renvoie true alors que
-     la spécification précise qu'elle devrait
-     renvoyer false.
+     la spécification précise qu'eil devrait
+     renvoyer false dans le cas niveau>max
      *******************************************/
     
     public void testDépartEgalMax() {
 	Jauge jaugeTest = new Jauge(42,42);
 	
-	assert jaugeTest.estVert() : "Erreur verte";
+	//assert !jaugeTest.estVert() : "Erreur verte";
 	assert jaugeTest.estRouge() : "Erreur rouge";
     }
+    /*******************************************
+     Rapport de Bogue sur testDépartEgalMax() :
+     L'appel de estVert() renvoie true alors que
+     la spécification précise qu'il devrait
+     renvoyer false dans le cas niveau==max.
+    *******************************************/
+    
+    public void testDépartNégatif() {
+	Jauge jaugeTest = new Jauge(-4,24);
+
+	//assert !jaugeTest.estVert() : "Erreur verte";
+	//assert !jaugeTest.estRouge() : "Erreur rouge";
+    }
+    /*******************************************
+     Rapport de Bogue sur testDépartNégatif() :
+     L'appel de estVert() renvoie true alors que
+     la spécification précise qu'il devrait
+     renvoyer false dans le cas niveau<0.
+     L'appel de estRouge() renvoie true alors
+     que la spécification précise qu'il devrait
+     renvoyer false dans le cas niveau<max.
+    *******************************************/
+
+    public void testDépartNul() {
+	Jauge jaugeTest = new Jauge(0,54);
+	
+	assert jaugeTest.estVert() : "Erreur verte";
+	//assert !jaugeTest.estRouge() : "Erreur rouge";
+    }
+    /*******************************************
+     Rapport de Bogue sur testDépartNul() :
+     L'appel de estRouge() renvoie true alors que
+     la spécification précise qu'il devrait
+     renvoyer false dans le cas niveau<test.
+    *******************************************/
+
+    //J'avoue que j'ai un problème là : je peux pas trop m'amuser à tester incrémenter et décrémenter à partir de estRouge() et estVert() puisque je sais que les fonctions sont complètement buggées et que je peux pas les corriger, mais dans le même temps j'ai pas grand chose d'autre
+    public void testDecrementer() {
+	Jauge jaugeTest = new Jauge(42,42);
+	jaugeTest.decrementer();
+
+	assert !jaugeTest.estRouge() : "Erreur dans decrementer()";
+    }
+
+    public void testIncrementer() {
+	Jauge jaugeTest = new Jauge(64,64);
+	jaugeTest.incrementer();
+
+	//assert jaugeTest.estRouge() : "Erreur dans incrementer()";
+    }
+    /*******************************************
+     Rapport de Bogue sur incrementer() :
+     L'appel de estRouge() renvoie false alors que
+     la spécification précise qu'il devrait
+     renvoyer false dans le cas niveau<test.
+     Un appel de toString() révèle que niveau
+     est à 0, laissant supposer un overflow.
+    *******************************************/
+
 }	
 	
