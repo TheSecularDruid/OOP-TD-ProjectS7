a20566daf0116ac41f1b28bcbc7eae0ddba25dcb
FELIX Esther <Esther.Felix@bordeaux-inp.fr>
2019-10-04 09:25:23 +0200

    Ajout des repertoires src, tst et build

---
+++ TD3/src/PassagerStandard.java
@@ -0,0 +1,71 @@
+
+public class PassagerStandard
+extends java.lang.Object
+implements Passager {
+    
+    final private String nom;
+    final private int destination;
+    private Position position;    
+    
+    public PassagerStandard (java.lang.String nom, int destination) {
+	this.nom = nom;
+	this.destination = destination;
+	this.position = new Position(); // dehors
+
+	
+    }
+
+    public java.lang.String nom() {
+	return this.nom;
+    }
+
+    public boolean estDehors() {
+	return (position.estDehors());
+    }
+
+    public boolean estAssis() {
+	return (position.estAssis());
+    }
+
+    public boolean estDebout() {
+	return (position.estDebout());
+    }
+
+    public void changerEnDehors() {
+	position = position.dehors();
+
+    }
+
+    public void changerEnAssis() {
+	position = position.assis();
+
+    }
+
+    public void changerEnDebout() {
+	position = position.debout();
+
+    }
+
+    public void monterDans(Vehicule v) {
+	if (v.aPlaceAssise()) {
+		v.monteeDemanderAssis(this);
+		position = position.assis();
+	} else if (v.aPlaceDebout()) {
+	    v.monteeDemanderDebout(this);
+	    position = position.debout();
+	}
+    }		
+
+    public void nouvelArret(Vehicule v, int numeroArret) {
+	if (numeroArret == destination) {
+	    v.arretDemanderSortie(this);
+	    position = position.dehors();
+	}
+
+    }
+
+    public java.lang.String toString() {
+	return "<" + nom + ">" + "<" + destination + ">" + "<" + position + ">";
+    }
+}
+ 
