5c36db184d6ab7aedae1945a522724451374a0d6
Gilliot Raphael <raphael.gilliot@bordeaux-inp.fr>
2019-10-23 13:03:35 +0200

    Refactorized the code to create new types of passengers with new behaviours Created an abstract class PassagerAbstrait to factorize all passengers internally

--- TD5/src/interface/PassagerStresse.java
+++ TD5/src/interface/PassagerStresse.java
@@ -1,22 +1,22 @@
 package tec;
 
-class PassagerStandard extends PassagerAbstrait {
+class PassagerStresse extends PassagerAbstrait {
     
-    public PassagerStandard(java.lang.String nom, int destination) {
+    public PassagerStresse(java.lang.String nom, int destination) {
 	super(nom, destination);
     }
-    
+
     protected void choixPlaceMontee(Vehicule v) {
 	if (v.aPlaceAssise())
 	    v.monteeDemanderAssis(this);
 	else if (v.aPlaceDebout())
 	    v.monteeDemanderDebout(this);
-	
     }
-	
+
     protected void choixPlaceArret(Vehicule v, int numeroArret) {
-	if (numeroArret>=this.destination)
+	if (this.estAssis()&&v.aPlaceDebout()&&(this.destination-numeroArret<=3))
+	    v.arretDemanderDebout(this);
+	if (numeroArret==this.destination)
 	    v.arretDemanderSortie(this);
     }
 }
-
