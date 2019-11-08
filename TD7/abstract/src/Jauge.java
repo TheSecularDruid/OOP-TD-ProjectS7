869f4797b407b7a1d3370d2097601161918f614d
Esther Felix <efelix@enseirb-matmeca.fr>
2019-11-08 09:13:16 +0100

    Added Jauge test

--- TD7/abstract/src/Jauge.java
+++ TD7/abstract/src/Jauge.java
@@ -1,23 +1,28 @@
 package tec;
 
-public class Jauge { 
+public class Jauge {
   private int valeur;
   private final int MAX;
 
   /**
-   * Construit une instance en précisant la valeur max de l'intervalle 
-   * [0, max[ et la position de départ de la Jauge. 
+   * Construit une instance en précisant la valeur max de l'intervalle
+   * [0, max[ et la position de départ de la Jauge.
    *
-   * La position de départ n'est pas forcément dans l'intervalle 
+   * La position de départ n'est pas forcément dans l'intervalle
    * [0, max[.
    *
    * @param max valeur maximale > 0 de l'intervalle.
    * @param depart   position de départ de la Jauge.
    */
-  public Jauge(int max, int depart) {
+  public Jauge(int max, int depart)
+    throws IllegalArgumentException {
+    if (max < 0) {
+      throw new IllegalArgumentException("vigie max < 0");
+    } else {
     valeur = depart;
     MAX = max;
   }
+}
 
   /**
    * La position de la jauge est-elle au-dessus de l'intervalle ?
@@ -56,12 +61,12 @@ public class Jauge {
 
   /**
    * Cette méthode est héritée de la classe {@link java.lang.Object}.
-   * Très utile pour le débogage, elle permet de fournir une 
+   * Très utile pour le débogage, elle permet de fournir une
    * chaîne de caractères correspondant a l'état d'un objet.
    * Mais, il faut adapter son code à chaque classe.
    */
   @Override
   public String toString() {
-    return "<" + valeur + " [0," + MAX + "[>"; 
+    return "<" + valeur + " [0," + MAX + "[>";
   }
 }
